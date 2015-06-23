23dfedb598a5b4bee3a21fef237a7d867259082c
Jonathan Perl <perl.jonathan@gmail.com>
2015-06-23 16:25:18 -0400

    Init commit

---
+++ README.md
@@ -0,0 +1,28 @@
+View Extensions [![Build Status](https://travis-ci.org/DispatchMe/meteor-view-extensions.svg?branch=master)](https://travis-ci.org/DispatchMe/meteor-viewport)
+======
+
+##Usage
+
+`meteor add dispatch:view-extensions`
+
+```
+var view = Template.myTemplate.constructView();
+
+// Here are a few extension methods you can use
+
+// Render the view without attaching it
+view.render();
+
+// Access the dom nodes
+view.nodes();
+
+// Attach the view to a dom node or another view.
+view.attach(document.body);
+
+// Set the data context
+view.setData({ foo: "bar" });
+
+// Extend the data context without overwriting it
+view.setData({ extended: true }, true);
+
+```
---
+++ package.js
@@ -0,0 +1,42 @@
+Package.describe({
+  name: 'dispatch:view-extensions',
+  summary: 'Extend meteor views with some useful helper functions.',
+  git: 'https://github.com/DispatchMe/meteor-view-extensions.git',
+  version: '0.0.1'
+});
+
+Package.onUse(function (api) {
+  api.versionsFrom('1.0');
+
+  api.use([
+    // core
+    'blaze',
+    'reactive-var',
+    'templating',
+    'tracker',
+
+    // atmosphere
+    'dispatch:blaze-emitter@0.0.4',
+    'raix:eventemitter@0.1.2'
+  ], 'web');
+
+  api.addFiles([
+    'view_extensions.js',
+  ], 'web');
+
+  api.export('Viewport', 'web');
+});
+
+Package.onTest(function (api) {
+  api.use([
+    'templating',
+    'test-helpers',
+    'tinytest',
+    'dispatch:view-extensions@0.0.1'
+  ], 'web');
+
+  api.addFiles([
+    'tests/templates.html',
+    'tests/view_extensions.js'
+  ], 'web');
+});
